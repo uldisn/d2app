4584f7658277f56c27709f65761a9d829697a4f6
Uldis Nelsons <uldis@weberp.lv>
2015-04-16 19:35:33 +0300

    Update main.php
    runtime directory moved to root

--- config/main.php
+++ config/main.php
@@ -23,6 +23,7 @@ $baseUrl              = (dirname($_SERVER['SCRIPT_NAME']) == '/' || dirname($_SE
 // main application configuration
 $mainConfig = array(
     'basePath'   => $applicationDirectory,
+    'runtimePath' => $applicationDirectory . '/../../../runtime',    
     'name'       => 'D2APP',
     'theme'      => 'frontend', // theme is copied eg. from vendor/p3bootstrap
     'language'   => 'en', // default language, see also components.langHandler
