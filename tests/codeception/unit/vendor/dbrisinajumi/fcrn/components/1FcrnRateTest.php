02f689f474495e38ce2ebe142240fc5c8df66bb0
Uldis <uldisnelsons@gmail.com>
2015-11-08 12:12:20 +0200

    Remove unused directories

--- tests/codeception/unit/vendor/dbrisinajumi/fcrn/components/1FcrnRateTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class FcrnRateTest extends CDbTestCase {
-
-    public function testApprove() {
-        return true;
-    }
-
-}
--- themes/backend2/views/skins/TbBreadcrumbs.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'default' => array(
-        'homeLink' => CHtml::link("Backend",Yii::app()->controller->createUrl('/p3admin'))
-    )
-);
-
-?>
